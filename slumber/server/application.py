29e3fa37583647e79a4093207f95fce6a8dc6766
Your Name <you@example.com>
2016-02-20 18:21:22 +0200

    checking if metaclass has app_label to prevent errors with mongoengine documents

--- slumber/server/application.py
+++ slumber/server/application.py
@@ -15,7 +15,7 @@ class DjangoApp(object):
         if hasattr(self.module, 'models'):
             for name in self.module.models.__dict__.keys():
                 potential = getattr(self.module.models, name)
-                if hasattr(potential, '_meta') and (
+                if hasattr(potential, '_meta') and hasattr(potential._meta, 'app_label') and (
                         appname == potential._meta.app_label or
                         appname.endswith('.' + potential._meta.app_label)):
                     model = DjangoModel(self, potential)
